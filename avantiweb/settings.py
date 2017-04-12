6c94edf1c2ec2bd4185bc16e337341ec9d7610ab
pauljherrera <pauljherrera@gmail.com>
2017-04-12 18:06:53 -0300

    Heroku

--- avantiweb/settings.py
+++ avantiweb/settings.py
@@ -253,3 +253,11 @@ REST_FRAMEWORK = {
     ]
 }
 
+# Heroku settings.
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
--- avantiweb/wsgi.py
+++ avantiweb/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "avantiweb.settings")
 
 application = get_wsgi_application()
+
+# Heroku settings.
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
