4670e11529d99dc68786e9dc65d43a7b4484a152
pauljherrera <pauljherrera@gmail.com>
2017-06-09 11:00:36 -0300

    Environment variables

--- avantiweb/settings.py
+++ avantiweb/settings.py
@@ -98,23 +98,13 @@ WSGI_APPLICATION = 'avantiweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'avantiweb',
-        'USER': 'postgres',
-        'PASSWORD': 'wwwpaul26N',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation
